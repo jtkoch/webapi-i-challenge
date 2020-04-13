35e44d669a85f9902d6f680caffa9ed57a681c4f
Jensen Koch <jtkoch12@gmail.com>
2020-04-13 12:57:43 -0500

    up to date with middleware training kit video

--- index.js
+++ index.js
@@ -1,33 +1,94 @@
-// require the express npm module, needs to be added to the project using "npm install express"
 const express = require('express');
-
-// creates an express application using the express module
+const port = 5000;
 const server = express();
+server.use(express.json());
 
-// configures our server to execute a function for every GET request to "/"
-// the second argument passed to the .get() method is the "Route Handler Function"
-// the route handler function will run on every GET request to "/"
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get('Origin')}`);
+  next();
+}
+
+function atGate(req, res, next) {
+  console.log('At the gate, about to be eaten');
+
+  next();
+}
+
+function auth(req, res, next) {
+  if(req.url === '/mellon') {
+    next();
+  } else {
+    res.send("You shall not pass!");
+  }
+}
+
+server.use(logger);
+server.use(atGate);
+
+server.get('/mellon', auth, (req, res) => {
+  console.log('Gate opening... ');
+  console.log('Inside and safe!');
+  res.send('Welcome Traveler!');
+})
+
 server.get('/', (req, res) => {
-  // express will pass the request and response objects to this function
-  // the .send() on the response object can be used to send a response to the client
   res.send('Hello World!');
 });
 
 server.get('/hobbits', (req, res) => {
+  const sortField = req.query.sortby || 'id';
   const hobbits = [
     {
       id: 1, 
       name: 'Samwise Gamgee'
     },
     {
+      id: 3, 
+      name: 'Bilbo Baggins'
+    }, 
+    {
       id: 2,
       name: 'Frodo Baggins'
     }
   ];
 
-  res.status(200).json(hobbits);
+  const response = hobbits.sort((a, b) => 
+    a[sortField] < b[sortField] ? -1 : 1
+  );
+
+res.status(200).json(response); 
 });
 
-// once the server is fully configured we can have it "listen" for connections on a particular "port"
-// the callback function passed as the second argument will run once when the server starts
-server.listen(8000, () => console.log('API running on port 8000'));
+  let hobbits = [
+    {
+      id: 1, 
+      name: 'Bilbo Baggins', 
+      age: 111
+    },
+    {
+      id: 2,
+      name: 'Frodo Baggins',
+      age: 33
+    }
+  ];
+  let nextId = 3;
+
+server.post('/hobbits', (req, res) => {
+  res.status(201).json({ url: '/hobbits', operation: 'POST' });
+});
+
+server.put('/hobbits', (req, res) => {
+  res.status(200).json({ url: '/hobbits', operation: 'PUT' });
+});
+
+server.delete('/hobbits/:id', (req, res) => {
+  res.status(204);
+});
+
+server.use(function(req, res) {
+  res.status(404).send(`Ain't nobody got time for dat!`);
+});
+
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
